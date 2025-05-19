852355f8d4a905320c2ca793078ff336c1799884
Fatemeh S.Moghadam <71309528+So-mo-on@users.noreply.github.com>
2025-05-19 03:28:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 # Create a selection box for the options
-option = st.selectbox("Select the Network Type", ("Coauthorship", "Citation"))
+option = st.selectbox("Select the Network Type", ("Coauthorship", "Paper network"))
 
 # Coauthorship page
 if option == "Coauthorship":
@@ -36,12 +36,22 @@ if option == "Coauthorship":
     """)
 
 # # Citation page (or other options)
-# elif option == "Citation":
-#     st.header("Citation Query")
-#
-#     # Get user input for citations (you can expand this)
-#     citation_query = st.text_input("Search for citation:")
-#     st.write(f"Search results for citations related to: {citation_query}")
-#
+elif option == "Paper network":
+    # st.header("Citation Query")
+    query = st.text_input("Search by keyword, research topic, field name, or researcher to explore collaboration patterns.")
+    n = st.number_input("How many results would you like to include in the network?", min_value=0, max_value=100)
+
+    if st.button("Results"):
+        if query and n:
+            functions.visualize_giant_component_paper(query, n)
+        else:
+            st.write("Please enter both your search query and number of results.")
+
+    st.markdown("""  
+    - **Node size** reflects a researcher's degree (number of collaborations).  
+    - **Link thickness** indicates the number of collaborations between two researchers.  
+    - You can **drag nodes** to rearrange the layout and explore the structure more easily!
+    """)
+
 
 
